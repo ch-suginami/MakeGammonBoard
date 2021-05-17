7042b6caa41d445569099483ccb61322381c916b
ch-suginami <67103794+ch-suginami@users.noreply.github.com>
2021-05-17 16:04:04 +0900

    Change license from GPL3.0 to MIT

--- MakeGammonBoard.py
+++ MakeGammonBoard.py
@@ -1,22 +1,11 @@
 '''
-  MakeGammonBoard -Making a figure of any backgammon board condition-
-  Copyright (C) 2021  Masanori Hirata(ch_suginami)
+MakeGammonBoard
+  --Making any situation figure of backgammon game.--
 
-  This program is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
+Copyright (c) 2021 Masanori Hirata(ch-suginami)
 
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-  Mail: ch.suginami.dp@gmail.com
-  Twitter: @ch_suginami
+This software is released under the MIT License.
+http://opensource.org/licenses/mit-license.php
 '''
 
 from PIL import Image, ImageDraw
